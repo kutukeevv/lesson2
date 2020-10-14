f87026dea6376ba8927c226308f4054ca7399f54
kutukeevv <zakuta18@gmail.com>
2020-10-14 21:13:22 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int num;
+	switch(num = 4){
+        case 1:
+            System.out.println("Число равно одному");
+            break;
+        case 2:
+            System.out.println("Число равно двум");
+            break;
+        default:
+            System.out.println("Число неизвестно");
+
+    }
+    }
+}
